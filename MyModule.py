2b1361b06464f02816fe277bdbd4bdbc9b0b3f23
VoTranQuyWyatt <wyatt@enclave.vn>
2019-03-01 16:55:07 +0700

    Module

---
+++ MyModule.py
@@ -0,0 +1,18 @@
+import module 
+import platform
+module.greeting("Wyaat")
+
+a=module.person1["age"]
+print(a)
+
+a=module.person1["name"]
+print(a)
+
+
+x=platform.system()
+print(x)
+
+# x=dir(platform)
+# print(x)
+
+
---
+++ module.py
@@ -0,0 +1,9 @@
+#Create a Module
+def greeting(name):
+    print("Hello "+name)
+
+person1 ={
+    "name":"John",
+    "age":36,
+    "country":"Iceland"
+}
